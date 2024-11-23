9cee9005d5cb2b54779471f461440632bb7e46eb
Artem0904 <moonportal708@gmail.com>
2024-11-23 16:41:03 +0200

    Add Base entities

---
+++ Rozetka/DataAccess/Data/Entities/Filter/Filter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Entities.Filter
+{
+    public class Filter :BaseNameEntity
+    {
+        public ICollection<FilterValue> Values { get; set; } = new HashSet<FilterValue>();
+        public ICollection<CategoryFilter> Filters { get; set; } = new HashSet<CategoryFilter>();
+    }
+}
---
+++ Rozetka/DataAccess/Data/Entities/Image.cs
@@ -0,0 +1,11 @@
+﻿
+namespace BusinessLogic.Entities
+{
+    public class Image
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Priority { get; set; }
+        public int AdvertId { get; set; }
+        public Advert Advert { get; set; }
+    }
+}
