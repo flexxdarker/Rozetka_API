dfdbe536f81a763bab169ef56511ba258bf876cf
flexxdarker <ksenzhakmax@gmail.com>
2025-03-24 21:10:51 +0200

    everything works correct

--- Rozetka/BusinessLogic/DTOs/Order/BasketViewItem.cs
+++ Rozetka/BusinessLogic/DTOs/Order/BasketViewItem.cs
@@ -12,5 +12,7 @@ namespace BusinessLogic.DTOs.Order
     {
         public int Id { get; set; }
         public List<BasketItemInformationDto> Items { get; set; } = new HashSet<BasketItemInformationDto>().ToList();
+        public DateTime? CreateTime { get; set; }
+        public decimal? Amount { get; set; }
     }
 }
